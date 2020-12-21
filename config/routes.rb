603d612315474bd62c6a8bc93291f6b9e777d712
Catalina <caliste@fen.uchile.cl>
2020-12-21 19:03:23 -0300

    design improvements

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,12 @@ Rails.application.routes.draw do
   resources :groups
   devise_for :users
 
-
-  root 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  unauthenticated :user do
+    root 'home#index'
+  end
+  
+  authenticated :user do
+    root "orders#new"
+  end
+  
 end
