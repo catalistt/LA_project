8026e0336bbfd56dc1045dbd7610fd4715851854
nayness <nayness@gmail.com>
2021-01-08 13:48:12 -0300

    Fix ajax calls

--- db/schema.rb
+++ db/schema.rb
@@ -110,7 +110,6 @@ ActiveRecord::Schema.define(version: 2021_01_07_181320) do
     t.float "discount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["group_id", "product_id"], name: "index_group_discounts_on_group_id_and_product_id", unique: true
     t.index ["group_id"], name: "index_group_discounts_on_group_id"
     t.index ["product_id"], name: "index_group_discounts_on_product_id"
   end
@@ -187,7 +186,6 @@ ActiveRecord::Schema.define(version: 2021_01_07_181320) do
     t.datetime "second_expiration_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["product_id", "supplier_id"], name: "index_purchases_on_product_id_and_supplier_id", unique: true
     t.index ["product_id"], name: "index_purchases_on_product_id"
     t.index ["supplier_id"], name: "index_purchases_on_supplier_id"
   end
@@ -226,33 +224,6 @@ ActiveRecord::Schema.define(version: 2021_01_07_181320) do
     t.string "rut"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "taggings", force: :cascade do |t|
-    t.integer "tag_id"
-    t.string "taggable_type"
-    t.integer "taggable_id"
-    t.string "tagger_type"
-    t.integer "tagger_id"
-    t.string "context", limit: 128
-    t.datetime "created_at"
-    t.index ["context"], name: "index_taggings_on_context"
-    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
-    t.index ["tag_id"], name: "index_taggings_on_tag_id"
-    t.index ["taggable_id", "taggable_type", "context"], name: "taggings_taggable_context_idx"
-    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
-    t.index ["taggable_id"], name: "index_taggings_on_taggable_id"
-    t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
-    t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
-    t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer "taggings_count", default: 0
-    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
