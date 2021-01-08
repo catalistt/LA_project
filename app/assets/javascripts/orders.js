2f2a2a0f4f708f5970a82beaf691731e7d57bb30
Catalina <caliste@fen.uchile.cl>
2021-01-08 00:30:02 -0300

    Price and Packaging ajax working

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -2,16 +2,25 @@ $(document).on('turbolinks:load', function() {
   $(document).on('cocoon:after-insert', function(){
     $('.st-product').find("select").on('change', function() {
       var product_id= $('.st-product').find("select").val()
-      console.log(product_id);
         $.ajax({
           type:"GET",
-          url:"products/"+product_id+"/set_price",
+          url:"/products/"+product_id+"/set_price",
           dataType:"json",
           success:function(result){
-            alert(result);
-            console.log(result); 
+            var input_price = document.getElementById('p-price');
+            var input_packaging = document.getElementById('packaging');
+            input_price.value = result;
+            input_packaging.value = 0;
           }
       })
-    })
+    });
+    $('#quantity').on('change', function() {
+      var p_quatity = $(this).val()
+      console.log(p_quatity);
+    }); 
+    $('.discount').find("select").on('change', function() {
+      var p_discount= $('.discount').find("select").val()
+      console.log(p_discount);  
+    });
   })
 })
