0a7bf257ed43c01b02a4feaddfa639cbdac2927c
Catalina <caliste@fen.uchile.cl>
2021-01-08 23:17:55 -0300

    Push to heroku

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,7 +2,6 @@ class Client < ApplicationRecord
   belongs_to :user
   belongs_to :group
   has_many :orders
-  accepts_nested_attributes_for :orders, allow_destroy: true
 
   def to_s
     business_name
