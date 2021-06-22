e485f270734ffdea681ee841b5c4a60defb3e1b5
nayness <nayness@gmail.com>
2021-06-21 21:54:40 -0400

    Fix: for postgres heroku db

--- db/migrate/20201217232242_change_category_format_products.rb
+++ db/migrate/20201217232242_change_category_format_products.rb
@@ -1,6 +1,6 @@
 class ChangeCategoryFormatProducts < ActiveRecord::Migration[5.2]
   def up
-    change_column :products, :category, :integer
+    change_column :products, :category, 'integer USING CAST(category AS integer)'
   end
   def down
     change_column :products, :category, :string
