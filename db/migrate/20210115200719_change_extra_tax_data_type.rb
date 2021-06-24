6c8ca77d85b17a0359e58ccf503410d009ef427a
nayness <nayness@gmail.com>
2021-06-24 18:25:20 -0400

    Fix: comment unncessary migrations

--- db/migrate/20210115200719_change_extra_tax_data_type.rb
+++ db/migrate/20210115200719_change_extra_tax_data_type.rb
@@ -1,6 +1,5 @@
 class ChangeExtraTaxDataType < ActiveRecord::Migration[5.2]
-  def change
-   change_column :add_products, :extra_tax, :float
-  end
-  
+  # def change
+  #  change_column :add_products, :extra_tax, :float
+  # end
 end
