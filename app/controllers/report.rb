1b69e3050c4908ead7134d174da91d2f3a50b4fa
nayness <nayness@gmail.com>
2021-06-24 18:38:07 -0400

    Fix: report syntax error

--- app/controllers/report.rb
+++ app/controllers/report.rb
@@ -1,7 +1,6 @@
 class Report < ApplicationRecord
   include Discard::Model
 
-
   def item(text)
     @count ||= 0
     @count += 1
@@ -11,16 +10,15 @@ class Report < ApplicationRecord
   def order
     options[:order]
   end
-end
 
-report = MyReport.new File.join(__dir__, 'report.erb'), {
-  order: { id: 70 }
-}
-@printer << report.render
-@printer.cut!
-@printer.to_escpos
-File.open('/dev/usb/lp0', 'w:ascii-8bit') { |f| f.write(printer.to_escpos) }
-# @printer.to_escpos or @printer.to_base64 contains resulting ESC/POS data
-
-
+  def whatever
+    report = MyReport.new File.join(__dir__, 'report.erb'), {
+      order: { id: 70 }
+    }
+    @printer << report.render
+    @printer.cut!
+    @printer.to_escpos
+    File.open('/dev/usb/lp0', 'w:ascii-8bit') { |f| f.write(printer.to_escpos) }
+    # @printer.to_escpos or @printer.to_base64 contains resulting ESC/POS data
+  end
 end
