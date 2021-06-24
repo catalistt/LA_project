a67f4df2c1a977059a06474f00031b98c958e282
nayness <nayness@gmail.com>
2021-06-24 19:44:39 -0400

    Fix: extra tax migration

--- db/migrate/20210104204456_add_extra_tax_to_add_products.rb
+++ db/migrate/20210104204456_add_extra_tax_to_add_products.rb
@@ -1,5 +1,5 @@
 class AddExtraTaxToAddProducts < ActiveRecord::Migration[5.2]
-  # def change
-  #   add_reference :add_products, :extra_tax, foreign_key: true
-  # end
+  def change
+    add_column :add_products, :extra_tax, :float
+  end
 end
