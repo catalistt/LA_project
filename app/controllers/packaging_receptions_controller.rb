596fa44c9e7d49985c1fd9c0c7930c4c834180d3
Catalina <caliste@fen.uchile.cl>
2021-07-27 18:15:23 -0400

    Add: download Natalia

--- app/controllers/packaging_receptions_controller.rb
+++ app/controllers/packaging_receptions_controller.rb
@@ -3,7 +3,7 @@ class PackagingReceptionsController < InheritedResources::Base
   def packaging_resume
     @packaging_receptions_day = PackagingReception.where(created_at: Time.zone.now.beginning_of_day...zone.now.end_of_day) 
     @packaging_reception_deliveries = @packaging_receptions_day.pluck(:delivery_method_id)
-    @turns = PackagingReception.where(created_at: .zone.now.beginning_of_day..Time.zone.now.end_of_day).pluck(:turn)
+    @turns = PackagingReception.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).pluck(:turn)
     @turns_not_nil = @turns.reject { |n| n == nil }
     @total_turns = @turns_not_nil.count
   end
